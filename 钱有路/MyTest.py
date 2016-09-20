f4719af842ada28cfcff809532355c984f7e9827
nami <zhangjia12247268@163.com>
2016-09-20 14:07:12 +0800

    测试类

--- 钱有路/MyTest.py
+++ 钱有路/MyTest.py
@@ -1,1 +1,13 @@
 import nose
+import linecache
+from 钱有路 import Post_tools
+from nose.tools import *
+
+def my_setup_function():
+    global my_post
+    my_post = Post_tools.MYPost(config_file_path='config.ini')
+
+
+# /appfrontservice/app/token/getToke 获取Token
+@with_setup(my_setup_function)
+def test_token():
